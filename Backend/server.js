1c53f2adb141237b4ea0e6c9c146e52be833019e
Mustafe-Abdirahman <khadar3021@gmail.com>
2025-01-30 13:59:46 +0300

    Legened

--- Backend/server.js
+++ Backend/server.js
@@ -35,5 +35,5 @@ app.get("/", (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port at ${PORT}`);
 });
